91d0fd6155739cfb632b5da30d7cd6ccfd42a815
Codingking <Codingking@atguigu.com>
2025-06-28 19:34:22 +0800

    [开发]提交租约业务

--- web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -2,6 +2,7 @@ package com.atguigu.lease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableScheduling //启用定时任务
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
