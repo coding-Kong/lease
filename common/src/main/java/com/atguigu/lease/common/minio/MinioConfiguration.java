4410cbbebcd15a5afddd890b6c170fe89fb77e16
Codingking <Codingking@atguigu.com>
2025-06-29 19:29:01 +0800

    [开发]提交移动端后端初始化代码

--- common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
+++ common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -3,6 +3,7 @@ package com.atguigu.lease.common.minio;
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(MinioProperties.class)
+@ConditionalOnProperty(name = "minio.endpoint")
 public class MinioConfiguration {
 //    @Value("${minio.endpoint}")
 //    private String endpoint;
